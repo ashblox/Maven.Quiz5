8edcd40b7d652aa37baadff91dd2efdb1b5df232
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-08 11:45:50 -0400

    working on powersets

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,18 +1,26 @@
 package rocks.zipcode.io.quiz4.generics;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class GenericUtils {
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
-        return null;
+    public static <T extends Comparable> Iterable<? extends Iterable<T>> powerSet(Set<T> originalSet) {
+        Set<Set<T>> powerSet = new HashSet<>();
+        powerSet.add(new HashSet<>());
+        for (T element : originalSet) {
+            Set<Set<T>> previousSets = new HashSet<>(powerSet);
+            for (Set<T> subSet : previousSets) {
+                Set<T> innerSet = new HashSet<>(subSet);
+                innerSet.add(element);
+                powerSet.add(innerSet);
+            }
+        }
+        return powerSet;
     }
 
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(_... originalSet) {
+    public static <T extends Comparable> Iterable<? extends Iterable<T>> powerSet(T... originalSet) {
         return powerSet(new TreeSet<>(Arrays.asList(originalSet)));
     }
 }
