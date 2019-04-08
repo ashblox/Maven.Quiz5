2dbf89f9628bbeb959b701897c136a4855e11409
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-08 10:05:16 -0400

    finished string assembler

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,26 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
-    public StringAssembler(Character delimeter) {
+
+    private Character delimiter;
+    private StringBuilder sb;
+
+    public StringAssembler(Character delimiter) {
+        this.delimiter = delimiter;
+        this.sb = null;
     }
 
     public StringAssembler append(String str) {
-        return null;
+        if (sb == null) {
+            sb = new StringBuilder(str);
+        } else {
+            sb.append(delimiter);
+            sb.append(str);
+        }
+        return this;
     }
 
     public String assemble() {
-        return null;
+        return sb.toString();
     }
 }
