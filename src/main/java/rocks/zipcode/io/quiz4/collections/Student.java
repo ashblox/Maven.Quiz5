dcc4b42d2ff4f6bbcc3c4cc700f645276e3d3930
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-08 10:17:34 -0400

    completed collections

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,25 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+
+    private Integer id;
+    private Double totalStudyTime;
+
     public Student() {
         this(null);
+        this.totalStudyTime = 0.0;
     }
 
     public Student(Integer id) {
+        this.id = id;
+        this.totalStudyTime = 0.0;
     }
 
     public void learn(Double amountOfHours) {
+        totalStudyTime += amountOfHours;
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return totalStudyTime;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,40 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+
+    private List<Student> studentList;
+
+    public ZipCodeWilmington() {
+        studentList = new ArrayList<>();
+    }
+
     public void enroll(Student student) {
+        studentList.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return studentList.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        for (Student student : studentList) {
+            student.learn(numberOfHours);
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        Map<Student, Double> studyMap = new LinkedHashMap<>();
+        for (Student student: studentList) {
+            studyMap.put(student, student.getTotalStudyTime());
+        }
+        return studyMap;
     }
 }
