8f0d9a4d62177b6e67eca84851cdbdb1c50a2ff8
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-08 09:25:02 -0400

    palindrome eval working

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,39 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.*;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        ArrayList<String> substrings = getAllSubstrings(string);
+        Set<String> palindromesUnique = new LinkedHashSet<>();
+        for (int i = 0; i < substrings.size(); i++) {
+            if (isPalindrome(substrings.get(i))) {
+                palindromesUnique.add(substrings.get(i));
+            }
+        }
+        String[] palindromes = new String[palindromesUnique.size()];
+        return palindromesUnique.toArray(palindromes);
+    }
+
+    public static ArrayList<String> getAllSubstrings(String string) {
+        ArrayList<String> substrings = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j <= string.length(); j++) {
+                substrings.add(string.substring(i, j));
+            }
+        }
+        return substrings;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder sb = new StringBuilder(string);
+        return sb.reverse().toString();
     }
 }
